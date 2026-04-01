a2eea47734d07f19eba09ad6ece22cca235527d6
fjorelashanaj-hue <fjorela.shanaj@fshnstudent.info>
2026-04-01 23:20:22 +0200

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner inputKonsola = new Scanner(System.in);
+        
+        System.out.println("=== Gjuha Ime Interaktive ===");
+        System.out.println("Shkruaj komandat (p.sh: a = 5, b = a * 2, Afisho b).");
+        System.out.println("Shkruaj 'Mbyll' per te perfunduar.\n");
+
+        while (true) {
+            System.out.print(">> ");
+            String rresht = inputKonsola.nextLine().trim();
+
+            if (rresht.equalsIgnoreCase("Mbyll")) {
+                break;
+            }
+
+            if (rresht.isEmpty()) {
+                continue;
+            }
+
+            try {
+                if (rresht.endsWith(";")) {
+                    rresht = rresht.substring(0, rresht.length() - 1);
+                }
+
+                Instruction instruksioni = Parser.parseLine(rresht);
+                
+                instruksioni.exec();
+
+            } catch (Exception e) {
+                System.out.println("Gabim Sintakse: " + e.getMessage());
+            }
+        }
+        
+        System.out.println("\n=== Programi perfundoi ===");
+    }
+}
